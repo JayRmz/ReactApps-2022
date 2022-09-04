2bad6430b14ce4b07ff8889c43289f9b9367cfa0
Juan Manuel <juanmanuel@MacBook-Air-de-Sistemas.local>
2022-09-04 01:10:48 -0500

    created new Expense item component

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14)
+    },
+    {
+      id: 'e2',
+      title: 'new TV',
+      amount: 799.49,
+      date: new Date(2021, 2, 12)
+    },
+    {
+      id: 'e3',
+      title: 'Car insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28)
+    },
+    {
+      id: 'e4',
+      title: 'Xbox Controller',
+      amount: 350.5,
+      date: new Date(2022, 8, 31)
+    }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Let's get started</h2>
+      <p>This is also visible!</p>
+      {
+        expenses.map((expense, index) => (
+          <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} key={index} />
+        ))
+      }
+
     </div>
   );
 }
